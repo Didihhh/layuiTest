05f6658a21511f1ab7f5889d44da88a273dec903
lihaoran0 <273072924@qq.com>
2020-05-29 10:05:27 +0800

    生成实体类和mybatis接口

---
+++ src/main/java/com/aaa/biz/DeptBiz.java
@@ -0,0 +1,32 @@
+package com.aaa.biz;
+
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.aaa.entity.Dept;
+
+public interface DeptBiz {
+    int countByExample(DeptExample example);
+
+    int deleteByExample(DeptExample example);
+
+    int deleteByPrimaryKey(Integer deptId);
+
+    int insert(Dept record);
+
+    int insertSelective(Dept record);
+
+    List<Dept> selectByExample(DeptExample example);
+
+    Dept selectByPrimaryKey(Integer deptId);
+
+    int updateByExampleSelective(@Param("record") Dept record, @Param("example") DeptExample example);
+
+    int updateByExample(@Param("record") Dept record, @Param("example") DeptExample example);
+
+    int updateByPrimaryKeySelective(Dept record);
+
+    int updateByPrimaryKey(Dept record);
+}
---
+++ src/main/java/com/aaa/biz/RoleBiz.java
@@ -0,0 +1,31 @@
+package com.aaa.biz;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.aaa.entity.Role;
+
+public interface RoleBiz {
+    int countByExample(RoleExample example);
+
+    int deleteByExample(RoleExample example);
+
+    int deleteByPrimaryKey(Integer roleId);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    List<Role> selectByExample(RoleExample example);
+
+    Role selectByPrimaryKey(Integer roleId);
+
+    int updateByExampleSelective(@Param("record") Role record, @Param("example") RoleExample example);
+
+    int updateByExample(@Param("record") Role record, @Param("example") RoleExample example);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
